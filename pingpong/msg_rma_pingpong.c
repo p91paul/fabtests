d024d9e4b16e13d5565ce255025e30ca8b1a6245
Paolo Inaudi <p91paul@gmail.com>
2016-02-02 19:03:24 +0100

    Port fi_msg_rma_pingpong to latest fabtests

--- pingpong/msg_rma_pingpong.c
+++ pingpong/msg_rma_pingpong.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 Intel Corporation.  All rights reserved.
+ * Copyright (c) 2013-2015 Intel Corporation.  All rights reserved.
  *
  * This software is available to you under the BSD license below:
  *
@@ -42,327 +42,113 @@
 #include <rdma/fi_errno.h>
 #include <shared.h>
 
-#include <fi_ext_dpa.h>
-
 static enum ft_rma_opcodes op_type = FT_RMA_WRITE;
-static struct cs_opts opts;
-static int max_credits = 128;
-static int credits = 128;
-static char test_name[10] = "custom";
-static struct timespec start, end;
-static void *buf;
-static size_t buffer_size;
 struct fi_rma_iov remote;
 static uint64_t cq_data = 1;
-static enum fi_mr_mode mr_mode;
-
-static struct fi_info *hints;
-
-static struct fid_fabric *fab;
-static struct fid_pep *pep;
-static struct fid_domain *dom;
-static struct fid_ep *ep;
-static struct fid_eq *cmeq;
-static struct fid_cq *rcq, *scq;
-static struct fid_mr *mr;
-
-static uint64_t use_scq = 1;
-
-//dpa_local_interrupt_t interrupt;
-//dpa_remote_interrupt_t remoteInterrupt;
-unsigned int interruptNo = 123456;
-unsigned int nodeId;
-//dpa_error_t error;
 int barrier = 1;
-volatile int *written;
+volatile int* written;
 
-static int send_xfer(int size)
-{
-	struct fi_cq_data_entry comp;
-	int ret;
-    if (!use_scq) goto post;
-	while (!credits) {
-		ret = fi_cq_read(scq, &comp, 1);
-		if (ret > 0) {
-			goto post;
-		} else if (ret < 0 && ret != -FI_EAGAIN) {
-			if (ret == -FI_EAVAIL) {
-				cq_readerr(scq, "scq");
-			} else {
-				FT_PRINTERR("fi_cq_read", ret);
-			}
-			return ret;
-		}
-	}
-
-	credits--;
-post:
-	ret = fi_send(ep, buf, (size_t) size, fi_mr_desc(mr), 0, ep);
-	if (ret)
-		FT_PRINTERR("fi_send", ret);
-
-	return ret;
-}
-
-static int recv_xfer(int size)
-{
-	struct fi_cq_data_entry comp;
-	int ret;
-
-	do {
-		ret = fi_cq_read(rcq, &comp, 1);
-		if (ret < 0 && ret != -FI_EAGAIN) {
-			if (ret == -FI_EAVAIL) {
-				cq_readerr(rcq, "rcq");
-			} else {
-				FT_PRINTERR("fi_cq_read", ret);
-			}
-			return ret;
-		}
-	} while (ret == -FI_EAGAIN);
-
-	ret = fi_recv(ep, buf, buffer_size, fi_mr_desc(mr), 0, buf);
-	if (ret)
-		FT_PRINTERR("fi_recv", ret);
-
-	return ret;
-}
-
-static int read_data(size_t size)
-{
-	int ret;
-
-	ret = fi_read(ep, buf, size, fi_mr_desc(mr), 
-		      0, remote.addr, remote.key, ep);
-	if (ret) {
-		FT_PRINTERR("fi_read", ret);
-		return ret;
-	}
-
-	return 0;
-}
-
-static int write_data_with_cq_data(size_t size)
-{
-	int ret;
-
-	ret = fi_writedata(ep, buf, size, fi_mr_desc(mr),
-		       cq_data, 0, remote.addr, remote.key, ep);
-	if (ret) {
-		FT_PRINTERR("fi_writedata", ret);
-		return ret;
-	}
-	return 0;
-}
-
-static int write_data(size_t size)
-{
-	int ret;
-
-    const struct iovec iov = {
-      .iov_base = (void*) buf,
-      .iov_len = size
-    };
-    const struct fi_rma_iov rma_iov = {
-      .addr = sizeof(int),
-      .key = remote.key
-    };
-    const struct fi_msg_rma msg = {
-      .msg_iov = &iov,
-      .desc = NULL,
-      .iov_count = 1,
-      .rma_iov = &rma_iov,
-      .rma_iov_count = 1,
-    };
+static int write(void) {
+  int ret = 0`;
+  switch (op_type) {
+  case FT_RMA_WRITE:;
+	const struct iovec iov = {
+	  .iov_base = (void*) buf,
+	  .iov_len = opts.transfer_size
+	};
+	const struct fi_rma_iov rma_iov = {
+	  .addr = sizeof(int),
+	  .key = remote.key
+	};
+	const struct fi_msg_rma msg = {
+	  .msg_iov = &iov,
+	  .desc = NULL,
+	  .iov_count = 1,
+	  .rma_iov = &rma_iov,
+	  .rma_iov_count = 1,
+	};
 
 	ret = fi_writemsg(ep, &msg, FI_MORE);    
 	if (ret) {
-		FT_PRINTERR("fi_write", ret);
-		return ret;
+	  FT_PRINTERR("fi_write", ret);
+	  break;
 	}
 
 	ret = fi_write(ep, &barrier, sizeof(int), NULL, 0, 0, remote.key, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_write", ret);
-		return ret;
-	}
-	return 0;
-}
-
-static int sync_test(void)
-{
-	int ret;
-    if (use_scq){
-      ret = wait_for_data_completion(scq, max_credits - credits);
-      if (ret) {
-		return ret;
-      }
-    }
-	credits = max_credits;
-
-	ret = opts.dst_addr ? send_xfer(16) : recv_xfer(16);
-	if (ret) {
-		return ret;
-	}
-
-	return opts.dst_addr ? recv_xfer(16) : send_xfer(16);
-}
-
-static int wait_remote_writedata_completion(void)
-{
-	struct fi_cq_data_entry comp;
-	int ret;
-
-	do {
-		ret = fi_cq_read(rcq, &comp, 1);
-		if (ret < 0 && ret != -FI_EAGAIN) {
-			if (ret == -FI_EAVAIL) {
-				cq_readerr(rcq, "rcq");
-			} else {
-				FT_PRINTERR("fi_cq_read", ret);
-			}
-			return ret;
-		}
-	} while (ret == -FI_EAGAIN);
-
-	ret = 0;
-	if (comp.data != cq_data) {
-		fprintf(stderr, "Got unexpected completion data %" PRIu64 "\n",
-			comp.data);
-	}
-	assert(comp.op_context == buf || comp.op_context == NULL);
-	if (comp.op_context == buf) {
-		/* We need to repost the receive */
-		ret = fi_recv(ep, buf, buffer_size, fi_mr_desc(mr), 0, buf);
-		if (ret)
-			FT_PRINTERR("fi_recv", ret);
-	}
-
-	return ret;
-}
-
-static int write(void) {
-  int ret=0;
-  switch (op_type) {
-  case FT_RMA_WRITE:
-    ret = write_data(opts.transfer_size);
-    break;
+	if (ret)
+	  FT_PRINTERR("fi_write", ret);
+	break;
   case FT_RMA_WRITEDATA:
-    ret = write_data_with_cq_data(opts.transfer_size);
-    if (ret)
-      return ret;
-    ret = wait_remote_writedata_completion();
-    break;
+	ret = fi_writedata(ep, buf, opts.transfer_size, fi_mr_desc(mr),
+				       cq_data, 0, remote.addr, remote.key, ep);
+	if (ret)
+	  FT_PRINTERR("fi_writedata", ret);
+
+	ret = ft_rx(0);
+	break;
   case FT_RMA_READ:
-    ret = read_data(opts.transfer_size); 
-    break;
+	ret = fi_read(ep, buf, opts.transfer_size, fi_mr_desc(mr),
+				  0, remote.addr, remote.key, ep);
+	if (ret)
+	  FT_PRINTERR("fi_read", ret);
+	break;
   }
   if (ret)
-    return ret;
-  if (use_scq) {
-    ret = wait_for_data_completion(scq, 1);
-    if (ret)
-      return ret;
-  }
-  //DPATriggerInterrupt(remoteInterrupt, 0, &error);
+	return ret;
+
+  ret = ft_get_tx_comp(++tx_seq);
   return ret;
 }
+
 
 static void wait() {
   while (*written != barrier);
 }
-  
+
+
 static int run_test(void)
 {
 	int ret, i;
-    
-	ret = sync_test();
-    write_data(opts.transfer_size);
-    *written = 0;
-    if (use_scq)
-      wait_for_data_completion(scq, 1);
-	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	ret = ft_sync();
+	if (ret)
+		return ret;
+
+	ft_start();
+	*written = 0;
 	for (i = 0; i < opts.iterations; i++) {
-      if (opts.dst_addr) {
-        ret = write();
+	  if (opts.dst_addr) {
+		ret = write();
         if (ret) return ret;
-        wait();
+		wait();
       } else {
-        wait();
-        ret = write();
-        if (ret) return ret;
-      }
-      barrier++;
+		wait();
+		ret = write();
+		if (ret) return ret;
+	  }
+	  barrier++;
 	}
-	clock_gettime(CLOCK_MONOTONIC, &end);
+	ft_stop();
 
 	if (opts.machr)
-		show_perf_mr(opts.transfer_size, opts.iterations, &start, &end, 
+		show_perf_mr(opts.transfer_size, opts.iterations, &start, &end,
 				2, opts.argc, opts.argv);
 	else
-		show_perf(test_name, opts.transfer_size, opts.iterations, 
+		show_perf(test_name, opts.transfer_size, opts.iterations,
 				&start, &end, 2);
 
 	return 0;
 }
 
-static void free_lres(void)
-{
-	fi_close(&cmeq->fid);
-}
-
-static int alloc_cm_res(void)
-{
-	struct fi_eq_attr cm_attr;
-	int ret;
-
-	memset(&cm_attr, 0, sizeof cm_attr);
-	ret = fi_eq_open(fab, &cm_attr, &cmeq, NULL);
-	if (ret)
-		FT_PRINTERR("fi_eq_open", ret);
-
-	return ret;
-}
-
-static void free_ep_res(void)
-{
-	fi_close(&ep->fid);
-	fi_close(&mr->fid);
-	fi_close(&rcq->fid);
-	fi_close(&scq->fid);
-	free(buf);
-}
-
 static int alloc_ep_res(struct fi_info *fi)
 {
-	struct fi_cq_attr cq_attr;
 	uint64_t access_mode;
 	int ret;
 
-	buffer_size = opts.user_options & FT_OPT_SIZE ?
-			opts.transfer_size : test_size[TEST_CNT - 1].size;
-	buf = malloc(MAX(buffer_size, sizeof(uint64_t)));
-	if (!buf) {
-		perror("malloc");
-		return -1;
-	}
+	ret = ft_alloc_active_res(fi);
+	if (ret)
+		return ret;
 
-	memset(&cq_attr, 0, sizeof cq_attr);
-	cq_attr.format = FI_CQ_FORMAT_DATA;
-	cq_attr.wait_obj = FI_WAIT_NONE;
-	cq_attr.size = opts.iterations;
-	ret = fi_cq_open(dom, &cq_attr, &scq, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_cq_open", ret);
-		goto err1;
-	}
-
-	ret = fi_cq_open(dom, &cq_attr, &rcq, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_cq_open", ret);
-		goto err2;
-	}
-	
 	switch (op_type) {
 	case FT_RMA_READ:
 		access_mode = FI_REMOTE_READ;
@@ -373,124 +159,17 @@ static int alloc_ep_res(struct fi_info *fi)
 		break;
 	default:
 		assert(0);
-		ret = -FI_EINVAL;
-		goto err3;
+		return -FI_EINVAL;
 	}
-	ret = fi_mr_reg(dom, buf, MAX(buffer_size, sizeof(uint64_t)), 
-			access_mode, 0, 0, 0, &mr, NULL);
+	ret = fi_mr_reg(domain, buf, buf_size,
+			access_mode, 0, FT_MR_KEY, 0, &mr, NULL);
 	if (ret) {
 		FT_PRINTERR("fi_mr_reg", ret);
-		goto err3;
+		return ret;
 	}
     written = (volatile int*)fi_mr_desc(mr);
 
-	if (!cmeq) {
-		ret = alloc_cm_res();
-		if (ret)
-			goto err4;
-	}
-
 	return 0;
-
-err4:
-	fi_close(&mr->fid);
-err3:
-	fi_close(&rcq->fid);
-err2:
-	fi_close(&scq->fid);
-err1:
-	free(buf);
-	return ret;
-}
-
-static int bind_ep_res(void)
-{
-	int ret;
-
-	ret = fi_ep_bind(ep, &cmeq->fid, 0);
-	if (ret) {
-		FT_PRINTERR("fi_ep_bind", ret);
-		return ret;
-	}
-    if (use_scq) {
-      ret = fi_ep_bind(ep, &scq->fid, FI_SEND);
-      if (ret) {
-		FT_PRINTERR("fi_ep_bind", ret);
-		return ret;
-      }
-    }
-
-	ret = fi_ep_bind(ep, &rcq->fid, FI_RECV);
-	if (ret) {
-		FT_PRINTERR("fi_ep_bind", ret);
-		return ret;
-	}
-
-	ret = fi_enable(ep);
-	if (ret) {
-		FT_PRINTERR("fi_enable", ret);
-		return ret;
-	}
-	
-	/* Post the first recv buffer */
-	ret = fi_recv(ep, buf, buffer_size, fi_mr_desc(mr), 0, buf);
-	if (ret)
-		FT_PRINTERR("fi_recv", ret);
-
-	return ret;
-}
-
-static int server_listen(void)
-{
-	struct fi_info *fi;
-	int ret;
-
-	ret = fi_getinfo(FT_FIVERSION, opts.src_addr, opts.src_port, FI_SOURCE,
-			hints, &fi);
-	if (ret) {
-		FT_PRINTERR("fi_getinfo", ret);
-		return ret;
-	}
-
-	ret = fi_fabric(fi->fabric_attr, &fab, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_fabric", ret);
-		goto err0;
-	}
-
-	ret = fi_passive_ep(fab, fi, &pep, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_passive_ep", ret);
-		goto err1;
-	}
-
-	ret = alloc_cm_res();
-	if (ret)
-		goto err2;
-
-	ret = fi_pep_bind(pep, &cmeq->fid, 0);
-	if (ret) {
-		FT_PRINTERR("fi_pep_bind", ret);
-		goto err3;
-	}
-
-	ret = fi_listen(pep);
-	if (ret) {
-		FT_PRINTERR("fi_listen", ret);
-		goto err3;
-	}
-
-	fi_freeinfo(fi);
-	return 0;
-err3:
-	free_lres();
-err2:
-	fi_close(&pep->fid);
-err1:
-	fi_close(&fab->fid);
-err0:
-	fi_freeinfo(fi);
-	return ret;
 }
 
 static int server_connect(void)
@@ -501,9 +180,9 @@ static int server_connect(void)
 	ssize_t rd;
 	int ret;
 
-	rd = fi_eq_sread(cmeq, &event, &entry, sizeof entry, -1, 0);
+	rd = fi_eq_sread(eq, &event, &entry, sizeof entry, -1, 0);
 	if (rd != sizeof entry) {
-		FT_PRINTERR("fi_eq_sread", rd);
+		FT_PROCESS_EQ_ERR(rd, eq, "fi_eq_sread", "listen");
 		return (int) rd;
 	}
 
@@ -511,58 +190,47 @@ static int server_connect(void)
 	if (event != FI_CONNREQ) {
 		fprintf(stderr, "Unexpected CM event %d\n", event);
 		ret = -FI_EOTHER;
-		goto err1;
+		goto err;
 	}
 
-    nodeId = ((struct dpa_addr_t*)info->dest_addr)->nodeId;
-
-	mr_mode = info->domain_attr->mr_mode;
-	ret = fi_domain(fab, info, &dom, NULL);
+	ret = fi_domain(fabric, info, &domain, NULL);
 	if (ret) {
 		FT_PRINTERR("fi_domain", ret);
-		goto err1;
-	}
-
-
-	ret = fi_endpoint(dom, info, &ep, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_endpoint", -ret);
-		goto err1;
+		goto err;
 	}
 
 	ret = alloc_ep_res(info);
 	if (ret)
-		 goto err1;
+		 goto err;
 
-	ret = bind_ep_res();
+	ret = ft_init_ep();
 	if (ret)
-		goto err3;
+		goto err;
 
 	ret = fi_accept(ep, NULL, 0);
 	if (ret) {
 		FT_PRINTERR("fi_accept", ret);
-		goto err3;
+		goto err;
 	}
 
-	rd = fi_eq_sread(cmeq, &event, &entry, sizeof entry, -1, 0);
- 	if (rd != sizeof entry) {
-		FT_PRINTERR("fi_eq_sread", rd);
-		goto err3;
- 	}
+	rd = fi_eq_sread(eq, &event, &entry, sizeof entry, -1, 0);
+	if (rd != sizeof entry) {
+		FT_PROCESS_EQ_ERR(rd, eq, "fi_eq_sread", "accept");
+		ret = (int) rd;
+		goto err;
+	}
 
 	if (event != FI_CONNECTED || entry.fid != &ep->fid) {
 		fprintf(stderr, "Unexpected CM event %d fid %p (ep %p)\n",
 			event, entry.fid, ep);
  		ret = -FI_EOTHER;
- 		goto err3;
+ 		goto err;
  	}
- 
+
  	fi_freeinfo(info);
  	return 0;
 
-err3:
-	free_ep_res();
-err1:
+err:
  	fi_reject(pep, info->handle, NULL, 0);
  	fi_freeinfo(info);
  	return ret;
@@ -572,113 +240,60 @@ static int client_connect(void)
 {
 	struct fi_eq_cm_entry entry;
 	uint32_t event;
-	struct fi_info *fi;
 	ssize_t rd;
 	int ret;
-
-	ret = ft_getsrcaddr(opts.src_addr, opts.src_port, hints);
-	if (ret)
-		return ret;
 
 	ret = fi_getinfo(FT_FIVERSION, opts.dst_addr, opts.dst_port, 0, hints, &fi);
 	if (ret) {
 		FT_PRINTERR("fi_getinfo", ret);
-		goto err0;
-	}
-    nodeId = ((struct dpa_addr_t*)fi->dest_addr)->nodeId;
-
-	ret = fi_fabric(fi->fabric_attr, &fab, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_fabric", ret);
-		goto err1;
+		return ret;
 	}
 
-	mr_mode = fi->domain_attr->mr_mode;
- 	ret = fi_domain(fab, fi, &dom, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_domain", ret);
-		goto err2;
-	}
-
-	ret = fi_endpoint(dom, fi, &ep, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_endpoint", ret);
-		goto err3;
-	}
+	ret = ft_open_fabric_res();
+	if (ret)
+		return ret;
 
 	ret = alloc_ep_res(fi);
 	if (ret)
-		goto err3;
+		return ret;
 
-	ret = bind_ep_res();
+	ret = ft_init_ep();
 	if (ret)
-		goto err5;
+		return ret;
 
 	ret = fi_connect(ep, fi->dest_addr, NULL, 0);
 	if (ret) {
 		FT_PRINTERR("fi_connect", ret);
-		goto err5;
+		return ret;
 	}
 
- 	rd = fi_eq_sread(cmeq, &event, &entry, sizeof entry, -1, 0);
+ 	rd = fi_eq_sread(eq, &event, &entry, sizeof entry, -1, 0);
 	if (rd != sizeof entry) {
-		FT_PRINTERR("fi_eq_sread", rd);
+		FT_PROCESS_EQ_ERR(rd, eq, "fi_eq_sread", "connect");
 		return (int) rd;
 	}
 
  	if (event != FI_CONNECTED || entry.fid != &ep->fid) {
  		fprintf(stderr, "Unexpected CM event %d fid %p (ep %p)\n",
  			event, entry.fid, ep);
- 		ret = -FI_EOTHER;
- 		goto err1;
+ 		return -FI_EOTHER;
  	}
-
-	fi_freeinfo(fi);
-	return 0;
-
-err5:
-	free_ep_res();
-err3:
-	fi_close(&dom->fid);
-err2:
-	fi_close(&fab->fid);
-err1:
-	fi_freeinfo(fi);
-err0:
-	return ret;
-}
-
-static int exchange_addr_key(void)
-{
-	struct fi_rma_iov *rma_iov;
-
-	rma_iov = buf;
-	if (opts.dst_addr) {
-		rma_iov->addr = mr_mode == FI_MR_SCALABLE ?
-				0 : (uintptr_t) buf;
-		rma_iov->key = fi_mr_key(mr);
-		send_xfer(sizeof *rma_iov);
-		recv_xfer(sizeof *rma_iov);
-		remote = *rma_iov;
-	} else {
-		recv_xfer(sizeof *rma_iov);
-		remote = *rma_iov;
-
-		rma_iov->addr = mr_mode == FI_MR_SCALABLE ?
-				0 : (uintptr_t) buf;
-		rma_iov->key = fi_mr_key(mr);
-		send_xfer(sizeof *rma_iov);
-	}
 
 	return 0;
 }
 
 static int run(void)
 {
-	int i, ret = 0;
+	char *node, *service;
+	uint64_t flags;
+	int i, ret;
+
+	ret = ft_read_addr_opts(&node, &service, hints, &flags, &opts);
+	if (ret)
+		return ret;
 
 	if (!opts.dst_addr) {
-		ret = server_listen();
+		ret = ft_start_server();
 		if (ret)
 			return ret;
 	}
@@ -687,19 +302,11 @@ static int run(void)
 	if (ret)
 		return ret;
 
-	ret = exchange_addr_key();
+	ret = ft_exchange_keys(&remote);
 	if (ret)
 		return ret;
 
-    /*dpa_error_t error;
-    dpa_desc_t sd;
-    DPAOpen(&sd, 0, &error);
-    DPACreateInterrupt(sd, &interrupt, 0, &interruptNo, NULL, NULL, DPA_FLAG_FIXED_INTNO, &error);
-    do {
-      DPAConnectInterrupt(sd, &remoteInterrupt, nodeId, 0, interruptNo, DPA_INFINITE_TIMEOUT, 0, &error);
-    } while(error != DPA_ERR_OK);
-    */
-	if (!(opts.user_options & FT_OPT_SIZE)) {
+	if (!(opts.options & FT_OPT_SIZE)) {
 		for (i = 0; i < TEST_CNT; i++) {
 			if (test_size[i].option > opts.size_option)
 				continue;
@@ -716,28 +323,17 @@ static int run(void)
 			goto out;
 	}
 
-	sync_test();
-    if (use_scq) {
-      wait_for_data_completion(scq, max_credits - credits);
-      /* Finalize before closing ep */
-      ft_finalize(ep, scq, rcq, FI_ADDR_UNSPEC);
-    }
+	ft_sync();
+	ft_finalize();
 out:
-    /*DPADisconnectInterrupt(remoteInterrupt, 0, &error);
-    DPARemoveInterrupt(interrupt, 0, &error);
-    DPAClose(sd, 0, &error);*/
 	fi_shutdown(ep, 0);
-	free_ep_res();
-	if (!opts.dst_addr)
-		free_lres();
-	fi_close(&dom->fid);
-	fi_close(&fab->fid);
 	return ret;
 }
 
 int main(int argc, char **argv)
 {
 	int op, ret;
+
 	opts = INIT_OPTS;
 
 	hints = fi_allocinfo();
@@ -751,6 +347,7 @@ int main(int argc, char **argv)
 				op_type = FT_RMA_READ;
 			} else if (!strcmp(optarg, "writedata")) {
 				op_type = FT_RMA_WRITEDATA;
+				cq_attr.format = FI_CQ_FORMAT_DATA;
 			} else if (!strcmp(optarg, "write")) {
 				op_type = FT_RMA_WRITE;
 			} else {
@@ -773,13 +370,13 @@ int main(int argc, char **argv)
 
 	if (optind < argc)
 		opts.dst_addr = argv[optind];
-    opts.size_option = 0;
+
 	hints->ep_attr->type = FI_EP_MSG;
 	hints->caps = FI_MSG | FI_RMA;
 	hints->mode = FI_LOCAL_MR | FI_RX_CQ_DATA;
 
 	ret = run();
 
-	fi_freeinfo(hints);
+	ft_free_res();
 	return -ret;
 }
